bd2f612b76b8abf13bcc5ccd31a5708b900a5314
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-14 17:31:09 +0600

    c = a++;

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 var a = 5;
 var b = 7;
-a++;
-++b;
+// a++;
+// ++b;
+// console.log(a);
+// console.log(b);
+var c, d;
+c = a++;
+d = ++b;
+
+console.log(c);
 console.log(a);
+console.log(d);
 console.log(b);
-// var c, d;
