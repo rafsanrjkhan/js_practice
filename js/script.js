6b333dd441346296d0d41b6fb35519181fb6d0b5
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-14 13:40:23 +0600

    user prompt input

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// console.log("This is 'console' log 1");
+// console.log('This is "console" log 2');
+// console.log(68.14);
+// document.write(68.14);
+prompt("Enter Your Name: ");
--- script.js
+++
@@ -1,4 +0,0 @@
-console.log("This is console log 1");
-console.log("This is console log 2");
-
-document.write("Js file is connected");
