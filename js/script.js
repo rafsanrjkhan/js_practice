482a175c10019bf867efe6e105c5dda4477b3fca
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-14 17:25:48 +0600

    arithmetic operator

--- js/script.js
+++ js/script.js
@@ -1,58 +1,7 @@
-var a;
-a = 23;
-const b = 10;
+var a = 5;
+var b = 7;
+a++;
+++b;
+console.log(a);
 console.log(b);
-
-
-// // way 3
-// var _abc, hdhf;
-// _abc = "rafsan";
-// console.log(_abc);
-// hdhf = 12.23;
-// console.log(hdhf);
-
-// // way 2
-// var $a;
-// $a = 20;
-// console.log($a);
-
-// // way 1
-// var a = 20;
-// var b = a;
-// console.log(a);
-// console.log(b);
-
-
-// var a = 20;
-// var b = a;
-// console.log(a);
-// console.log(b);
-// a = 2;
-// b = 33;
-// console.log(a + b);
-// console.log(b);
-
-
-// var a;
-// let b;
-// a = 24;
-// b ="Rafsan";
-// document.write(a);
-// document.write(b);
-// b = "Ananta";
-// b = 12.12;
-// document.write(b);
-
-
-// var myage = 24;
-// document.write(myage);
-
-// var x;
-// x = prompt("Enter Your Name: ");
-// document.write(x);
-
-
-// console.log("This is 'console' log 1");
-// console.log('This is "console" log 2');
-// console.log(68.14);
-// document.write(68.14);
+// var c, d;
