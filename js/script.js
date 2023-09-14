8ccd430d0c39fc56b2147576e2bda3d7ee3f69d8
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-14 14:10:58 +0600

    var.let

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-var x;
-x = prompt("Enter Your Name: ");
-document.write(x);
+var a;
+let b;
+a = 24;
+b ="Rafsan";
+console.log(b);
+console.log(a);
+document.write(a);
+document.write(b);
+
+// var myage = 24;
+// document.write(myage);
+
+// var x;
+// x = prompt("Enter Your Name: ");
+// document.write(x);
 
 
 // console.log("This is 'console' log 1");
