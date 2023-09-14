962606f88b5c462c07c2de89f335b8a7e1cee613
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-14 13:52:01 +0600

    name print

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+var x;
+x = prompt("Enter Your Name: ");
+document.write(x);
+
+
 // console.log("This is 'console' log 1");
 // console.log('This is "console" log 2');
 // console.log(68.14);
 // document.write(68.14);
-prompt("Enter Your Name: ");
