be2e455fce8322132c76aea48add02312c424b22
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-14 16:23:26 +0600

    const

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
-// way 3
-var _abc, hdhf;
-_abc = "rafsan";
-console.log(_abc);
-hdhf = 12.23;
-console.log(hdhf);
-
-// way 2
-var $a;
-$a = 20;
-console.log($a);
-// way 1
-var a = 20;
-var b = a;
-console.log(a);
+var a;
+a = 23;
+const b = 10;
 console.log(b);
+
+
+// // way 3
+// var _abc, hdhf;
+// _abc = "rafsan";
+// console.log(_abc);
+// hdhf = 12.23;
+// console.log(hdhf);
+
+// // way 2
+// var $a;
+// $a = 20;
+// console.log($a);
+
+// // way 1
+// var a = 20;
+// var b = a;
+// console.log(a);
+// console.log(b);
 
 
 // var a = 20;
