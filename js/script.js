d22f2cd2126b363fb83e806c7ff63ba02b2bcaff
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-15 10:48:12 +0600

    artm priority

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-var a = 5;
-var b = 7;
-
-var c, d;
-c = a--;
-d = --b;
-
-console.log(c);
+// Priority
+a = 3 - 5 * 5 / 7;
 console.log(a);
-console.log(d);
+
+b = (3 - 5) * 5 / 7;
 console.log(b);
+
+c = (3 - 5) * 5 / 7 + 4**4;
+console.log(c);
+// Decrement a--/--a
+// var a = 5;
+// var b = 7;
+
+// var c, d;
+// c = a--;
+// d = --b;
+
+// console.log(c);
+// console.log(a);
+// console.log(d);
+// console.log(b);
 
 // Increment a++/++a
 // var a = 5;
