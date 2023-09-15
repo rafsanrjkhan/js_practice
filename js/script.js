cd17977e581a373cdcf28a7d9ac2ba854db14f79
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-15 12:39:21 +0600

    logical operator

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
+console.log();
+// Logical Operator
+var a = true;
+var b = false;
+var c = false;
+document.write(c || b || a);
+
 // Comparison Operator
-var a =4;
-var b =4;
-document.write(a == b);
+// var a =4;
+// var b =4;
+// document.write(a == b);
 
-document.write(" ");
+// document.write(" ");
 
-var c = 4;
-var d = 3;
-document.write(c == d);
+// var c = 4;
+// var d = 3;
+// document.write(c == d);
+// this operator alwys return true of false
