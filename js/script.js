9479e9753f58016b80401c46bf82a599114486f5
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-15 17:18:55 +0600

    boolean

--- js/script.js
+++ js/script.js
@@ -1,44 +1,68 @@
+console.log();
+10>9
+-1>2
+"Hello" == "Hello"
+var x = "Hello";
+var y = Boolean(x);
+
+x = null
+y = Boolean(x);
+
+x = 10 / "H";
+Boolean(x);
+
+var myVar;
+Boolean(x);
+
+//links
+//https://www.w3schools.com/jsref/jsref_obj_boolean.asp 
+
+
+// a = "rafsan";
+// a.replace("r", "d")
+// document.write(a);
+
 // Strings
-"Hello World"
-'Hello World'
-"Hello " World"
-"Hello  World"
-"Hello n World"
-'Hello ' World'
-'Hello t World'
+// "Hello World"
+// 'Hello World'
+// "Hello " World"
+// "Hello  World"
+// "Hello n World"
+// 'Hello ' World'
+// 'Hello t World'
 
 //Concatenation
-"Hello" + "World"
+// "Hello" + "World"
 
 
-var x = "Hello World";
+// var x = "Hello World";
 
 //Length of String
-x.length; //It is a property
+// x.length; //It is a property
 
 //Indexing
-"Hello World"[0]
-"Hello World"[3]
-x = "Hello World";
-x[0]
-x[3]
+// "Hello World"[0]
+// "Hello World"[3]
+// x = "Hello World";
+// x[0]
+// x[3]
 
 //Immutable
-x[1]
-x[1] = "5"
-x[1]
+// x[1]
+// x[1] = "5"
+// x[1]
 
 
 // return a new string, doesn't change x
 //These are methods
-x.slice(1, 5);
-x.slice(-6, -1);
-x.substr(3, 2); // 2nd parameter means the length
-x.replace("Hello", "World");
-x.toUpperCase();
-x.toLowerCase();
-x.concat("1", "2");
-x.trim();
+// x.slice(1, 5);
+// x.slice(-6, -1);
+// x.substr(3, 2); // 2nd parameter means the length
+// x.replace("Hello", "World");
+// x.toUpperCase();
+// x.toLowerCase();
+// x.concat("1", "2");
+// x.trim();
 
 
 //links
