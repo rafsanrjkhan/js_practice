e360a49a58e64e4539a8486725e3282381f6c8bc
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-15 10:38:19 +0600

    Decrement  a--/--a

--- js/script.js
+++ js/script.js
@@ -2,10 +2,23 @@ var a = 5;
 var b = 7;
 
 var c, d;
-c = a++;
-d = ++b;
+c = a--;
+d = --b;
 
 console.log(c);
 console.log(a);
 console.log(d);
 console.log(b);
+
+// Increment a++/++a
+// var a = 5;
+// var b = 7;
+
+// var c, d;
+// c = a++;
+// d = ++b;
+
+// console.log(c);
+// console.log(a);
+// console.log(d);
+// console.log(b);
