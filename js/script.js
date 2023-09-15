17f47cebd2ca3ece5b3749505acc58881ef51e96
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-15 14:47:39 +0600

    conditional operator

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
-console.log();
+// Conditional Operator
+var a = (1<8) ? "Hello":"World";
+document.write(a);
+
+document.write(" ");
+
+var b = (1>8) ? "Hello":"World";
+document.write(b);
+
+
 // Logical Operator
-var a = true;
-var b = false;
-var c = false;
-var d = (4>6);
-var e = (1<7);
-var f = (1<-9);
-document.write(c || b || a);
-document.write(" ");
-document.write(a && b);
-document.write(" ");
-document.write(d || e);
-document.write(" ");
-document.write(d || f);
+// var a = true;
+// var b = false;
+// var c = false;
+// var d = (4>6);
+// var e = (1<7);
+// var f = (1<-9);
+// document.write(c || b || a);
+// document.write(" ");
+// document.write(a && b);
+// document.write(" ");
+// document.write(d || e);
+// document.write(" ");
+// document.write(d || f);
 
 
 
