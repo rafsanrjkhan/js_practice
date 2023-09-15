9d17f4266451a481797fbbd1dab25a966192b185
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-15 11:37:01 +0600

    comparison operator

--- js/script.js
+++ js/script.js
@@ -1,49 +1,10 @@
-var a = "Rafsan";
-var b = 27;
-var c = 45;
-document.write(a + " " + b + c);
+// Comparison Operator
+var a =4;
+var b =4;
+document.write(a == b);
+
 document.write(" ");
-document.write(a + " " + (b + c));
 
-
-// Sting Operator
-// var a = "Rafsan";
-// var b = "Ananta";
-// var c = " Khan.";
-// document.write(a + " " + b + c);
-
-// Priority
-// a = 3 - 5 * 5 / 7;
-// console.log(a);
-
-// b = (3 - 5) * 5 / 7;
-// console.log(b);
-
-// c = (3 - 5) * 5 / 7 + 4**4;
-// console.log(c);
-
-// Decrement a--/--a
-// var a = 5;
-// var b = 7;
-
-// var c, d;
-// c = a--;
-// d = --b;
-
-// console.log(c);
-// console.log(a);
-// console.log(d);
-// console.log(b);
-
-// Increment a++/++a
-// var a = 5;
-// var b = 7;
-
-// var c, d;
-// c = a++;
-// d = ++b;
-
-// console.log(c);
-// console.log(a);
-// console.log(d);
-// console.log(b);
+var c = 4;
+var d = 3;
+document.write(c == d);
