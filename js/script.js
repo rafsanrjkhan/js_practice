c2def91a847dffd00018888f0d8090a0556302cc
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-19 12:38:23 +0600

    primitive type

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 console.log()
+// Premitive types
+// number, string, boolean
 
-student = {name:"Rahim", age:25, hometown:"Dhaka"}
-student["name"]
-student.name
+// deals with value
+let a = 7;
+let b = a; // b = 7
+a = 45;
+console.log(b);
 
 
-student = {}
-student["name"] = "Rahim"
 
 
-delete simanta.name 
+// student = {name:"Rahim", age:25, hometown:"Dhaka"}
+// student["name"]
+// student.name
+
+
+// student = {}
+// student["name"] = "Rahim"
+
+
+// delete simanta.name 
