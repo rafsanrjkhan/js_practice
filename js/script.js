ec06123c86deeb3e5be370a3985817ffa326fba3
rafsanrjkhan <rjkhan104@gmail.com>
2023-09-19 12:45:18 +0600

    reference types

--- js/script.js
+++ js/script.js
@@ -3,12 +3,21 @@ console.log()
 // number, string, boolean
 
 // deals with value
+// different address
 let a = 7;
 let b = a; // b = 7
 a = 45;
 console.log(b);
 
-
+// Refenrence type
+// array, object
+// address
+let numbers = [1, 2, 3]
+let newNumbers = numbers;
+console.log(newNumbers); // [1, 2, 3]
+numbers[1] = 500;
+console.log(numbers); // [1, 500, 3]
+console.log(newNumbers); // [1, 500, 3]
 
 
 // student = {name:"Rahim", age:25, hometown:"Dhaka"}
